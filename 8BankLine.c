5071bfb83ae475a36f49dcc9a828d5318a6c75f6
Joao Alonso <alonso963@hotmail.com>
2020-04-12 20:25:02 -0300

    primeiros exs lista 2 URI

---
+++ 8BankLine.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+
+    int n, i, j, soma, pn[j];
+    scanf ("%d", &n);
+    if (n == 3){
+        soma = 1;
+    } 
+    else {
+        
+        for (i = 3; i <= n; i++){
+            for (j = 0; j < n; j++);{
+                pn[j] = (n-j);
+            }   
+        }
+    }
+}
